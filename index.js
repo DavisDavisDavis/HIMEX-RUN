306d5c2da46edd88558247aa7b10503c622f696c
DavisDavisDavis <89775852+DavisDavisDavis@users.noreply.github.com>
2022-04-02 22:32:34 +0200

    Enemy class 🐱

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 250;
-var enemyX = 0;
+var Enemy = /** @class */ (function () {
+    function Enemy(speed, context) {
+        this.x = canvas.width;
+        this.speed = speed;
+        this.context = context;
+    }
+    Enemy.prototype.drawEnemy = function () {
+        context.fillStyle = "pink";
+        this.x -= this.speed;
+        if (0 >= this.x) {
+            this.x = canvas.width;
+        }
+        context.fillStyle = "pink";
+        context.fillRect(this.x, 60, 50, 50);
+    };
+    return Enemy;
+}());
+var enemy = new Enemy(1, context);
 (function animate() {
     context.fillStyle = "#a1a9fe";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = "pink";
-    function enemyMovement() {
-        enemyX -= 1;
-        if (0 >= enemyX) {
-            enemyX = canvas.width;
-        }
-        return enemyX;
-    }
-    context.fillRect(enemyMovement(), 50, 50, 50);
+    enemy.drawEnemy();
     window.requestAnimationFrame(animate);
 })();
--- index.ts
+++ index.ts
@@ -4,23 +4,37 @@ const context = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 250;
 
-let enemyX = 0;
+class Enemy {
+  x: number;
+  y: number;
+  speed: number;
+  context: CanvasRenderingContext2D;
 
+  constructor(speed: number, context: CanvasRenderingContext2D) {
+    this.speed = speed;
+    this.context = context;
+  }
+
+  x = canvas.width;
+  drawEnemy() {
+    context.fillStyle = "pink";
+
+    this.x -= this.speed;
+    if (0 >= this.x) {
+      this.x = canvas.width;
+    }
+
+    context.fillStyle = "pink";
+    context.fillRect(this.x, 60, 50, 50);
+  }
+}
+
+const enemy = new Enemy(1, context);
 (function animate() {
   context.fillStyle = "#a1a9fe";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  context.fillStyle = "pink";
-
-  function enemyMovement() {
-    enemyX -= 1;
-    if (0 >= enemyX) {
-      enemyX = canvas.width;
-    }
-    return enemyX;
-  }
-
-  context.fillRect(enemyMovement(), 50, 50, 50);
+  enemy.drawEnemy();
 
   window.requestAnimationFrame(animate);
 })();
