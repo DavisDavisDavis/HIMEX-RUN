1215fbec26295e4b8e839c3da563e51d863a173b
DavisDavisDavis <89775852+DavisDavisDavis@users.noreply.github.com>
2022-04-04 22:12:07 +0200

    Hime jump on click event 🎈

--- index.js
+++ index.js
@@ -8,16 +8,40 @@ var Hime = /** @class */ (function () {
         this.y = 10;
         this.height = 35;
         this.velocity = 2.4;
+        this.charge = 0;
+        window.addEventListener("click", this.jump.bind(this), false);
     }
-    Hime.prototype.movement = function (e) {
+    Hime.prototype.movement = function () {
         this.y += this.velocity;
-        if (this.y >= 100) {
+        if (this.y >= 140) {
             this.velocity = 0;
             this.airborne = false;
         }
-        if (e.click == true) {
-            console.log();
+        else {
+            this.velocity = 2.4;
         }
+    };
+    Hime.prototype.jump = function (e) {
+        //this.airborne = true;
+        if (!this.airborne && this.charge == 0) {
+            this.charge = 1;
+            this.velocity = -60;
+            this.airborne = true;
+        }
+        else if (this.charge > 0 && this.charge < 15) {
+            this.charge++;
+            console.log(this.charge);
+            this.velocity = -60 - this.charge / 50;
+        }
+        /*
+        if (this.grounded && this.jumpTimer == 0) {
+          this.jumpTimer = 1;
+          this.dy = -this.jumpForce;
+        } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+          this.jumpTimer++;
+          this.dy = -this.jumpForce - (this.jumpTimer / 50);
+        }
+        */
     };
     Hime.prototype.draw = function (context) {
         this.movement();
@@ -41,7 +65,7 @@ var Enemy = /** @class */ (function () {
     Enemy.prototype.draw = function (context) {
         this.movement();
         context.fillStyle = "pink";
-        context.fillRect(this.x, 100, 50, 50);
+        context.fillRect(this.x, 140, 50, 50);
     };
     return Enemy;
 }());
@@ -54,4 +78,3 @@ var enemy = new Enemy(1.2);
     enemy.draw(context);
     window.requestAnimationFrame(animate);
 })();
-window.addEventListener("click", hime.movement, false);
--- index.ts
+++ index.ts
@@ -9,6 +9,7 @@ class Hime {
   y: number;
   height: number;
   velocity: number;
+  charge: number;
   airborne: boolean;
   context: CanvasRenderingContext2D;
 
@@ -17,19 +18,42 @@ class Hime {
     this.y = 10;
     this.height = 35;
     this.velocity = 2.4;
+    this.charge = 0;
+    window.addEventListener("click", this.jump.bind(this), false);
   }
 
-  movement(e) {
+  movement() {
     this.y += this.velocity;
 
-    if (this.y >= 100) {
+    if (this.y >= 140) {
       this.velocity = 0;
       this.airborne = false;
+    } else {
+      this.velocity = 2.4;
+    }
+  }
+
+  jump(e: MouseEvent) {
+    //this.airborne = true;
+    if (!this.airborne && this.charge == 0) {
+      this.charge = 1;
+      this.velocity = -60;
+      this.airborne = true;
+    } else if (this.charge > 0 && this.charge < 15) {
+      this.charge++;
+      console.log(this.charge);
+      this.velocity = -60 - this.charge / 50;
     }
 
-    if (e.click == true) {
-      console.log();
+    /*
+    if (this.grounded && this.jumpTimer == 0) {
+      this.jumpTimer = 1;
+      this.dy = -this.jumpForce;
+    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
+      this.jumpTimer++;
+      this.dy = -this.jumpForce - (this.jumpTimer / 50);
     }
+    */
   }
 
   draw(context: CanvasRenderingContext2D) {
@@ -61,7 +85,7 @@ class Enemy {
   draw(context: CanvasRenderingContext2D) {
     this.movement();
     context.fillStyle = "pink";
-    context.fillRect(this.x, 100, 50, 50);
+    context.fillRect(this.x, 140, 50, 50);
   }
 }
 
@@ -77,5 +101,3 @@ const enemy = new Enemy(1.2);
 
   window.requestAnimationFrame(animate);
 })();
-
-window.addEventListener("click", hime.movement, false);
